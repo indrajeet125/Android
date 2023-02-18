cf85822c37f6c8fc721a74bf2f1af2a0175a618d
indrajeet125 <indrajeetyadav932001@gmail.com>
2023-02-19 03:03:02 +0530

    19-2-23

--- AndroidWithJava/DialCallingIntent/app/src/main/java/com/example/dialcallingintent/MainActivity.java
+++ AndroidWithJava/DialCallingIntent/app/src/main/java/com/example/dialcallingintent/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.dialcallingintent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onCall(View view) {
+        System.out.println("call button clicked --------------n\n\n\n");
+        EditText dial=findViewById(R.id.number);
+        String number= (dial.getText()).toString();
+        Intent intent =new Intent(Intent.ACTION_DIAL,  Uri.parse("tel" + "5673543534"));
+        startActivity(intent);
+
+
+
+//        Intent callIntent = new Intent(Intent.ACTION_CALL);
+//        callIntent.setData(Uri.parse("tel:"+number));
+//        startActivity(callIntent);
+    }
+
+    public void onDial(View view) {
+
+        Intent intent =new Intent(Intent.ACTION_DIAL);
+        startActivity(intent);
+
     }
 }
