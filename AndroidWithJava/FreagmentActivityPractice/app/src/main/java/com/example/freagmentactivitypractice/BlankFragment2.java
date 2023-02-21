85a6413d026c6b34aa4eb176882ee05723e89b86
indrajeet125 <indrajeetyadav932001@gmail.com>
2023-02-21 22:36:15 +0530

    date 21-02-23

---
+++ AndroidWithJava/FreagmentActivityPractice/app/src/main/java/com/example/freagmentactivitypractice/BlankFragment2.java
@@ -0,0 +1,28 @@
+package com.example.freagmentactivitypractice;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class BlankFragment2 extends Fragment {
+
+    public BlankFragment2() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_blank2, container, false);
+        TextView textView = view.findViewById(R.id.textView2);
+
+        return view;
+    }
+}
