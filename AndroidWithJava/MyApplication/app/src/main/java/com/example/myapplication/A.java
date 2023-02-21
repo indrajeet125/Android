66ff23bb19afdd60c668644db07d92d230059b45
indrajeet <indrajeetyadav932001@gmail.com>
2023-02-21 22:36:15 +0530

    date 21-02-23

---
+++ AndroidWithJava/MyApplication/app/src/main/java/com/example/myapplication/A.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class A extends Fragment {
+
+
+    public A() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_a, container, false);
+        TextView textView=view.findViewById(R.id.textView1);
+        return view;
+    }
+}
