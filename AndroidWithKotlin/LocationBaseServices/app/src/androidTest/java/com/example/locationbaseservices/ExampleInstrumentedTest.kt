3f847c0992f18817bb3d1915f6e8136d651d3888
indrajeet <indrajeetyadav932001@gmail.com>
2023-04-01 01:56:58 +0530

    practice location base services like google map enabling

--- AndroidWithKotlin/LocationBaseServices/app/src/androidTest/java/com/example/locationbaseservices/ExampleInstrumentedTest.kt
+++ AndroidWithKotlin/LocationBaseServices/app/src/androidTest/java/com/example/locationbaseservices/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.kotlinenable
+package com.example.locationbaseservices
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.kotlinenable", appContext.packageName)
+        assertEquals("com.example.locationbaseservices", appContext.packageName)
     }
 }
