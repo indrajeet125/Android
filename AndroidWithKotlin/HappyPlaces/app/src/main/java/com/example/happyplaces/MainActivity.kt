de4aa010537ca30c0c1454b5e7ff5d18aa286913
indrajeet125 <indrajeetyadav932001@gmail.com>
2023-04-02 23:42:05 +0530

    initial commit

--- AndroidWithKotlin/HappyPlaces/app/src/main/java/com/example/happyplaces/MainActivity.kt
+++ AndroidWithKotlin/HappyPlaces/app/src/main/java/com/example/happyplaces/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.happyplaces
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val fabAddHappyPlace = findViewById(R.id.fabAddHappyPlace) as FloatingActionButton
+        fabAddHappyPlace.setOnClickListener {
+            val intent = Intent(this, AddHappyPlaceActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
