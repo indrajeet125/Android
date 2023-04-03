72cdea66189abf061aebfd34cebac52d3d927b15
indrajeet125 <indrajeetyadav932001@gmail.com>
2023-04-03 15:04:21 +0530

    date Picker added

--- AndroidWithKotlin/HappyPlaces/app/src/main/java/com/example/happyplaces/AddHappyPlaceActivity.kt
+++ AndroidWithKotlin/HappyPlaces/app/src/main/java/com/example/happyplaces/AddHappyPlaceActivity.kt
@@ -1,20 +1,67 @@
 package com.example.happyplaces
 
+import android.app.DatePickerDialog
+import android.icu.lang.UCharacter.GraphemeClusterBreak.V
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.DatePicker
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.appcompat.widget.Toolbar
+import java.text.SimpleDateFormat
+import java.util.*
 
-class AddHappyPlaceActivity : AppCompatActivity() {
+class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
+    var et_date: AppCompatEditText? = null
+    private var cal = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_happy_place)
 
-//        val toolbar_add_place = findViewById(R.id.toolbar_add_place) as Toolbar
-//        setSupportActionBar(toolbar_add_place) // Use the toolbar to set the action bar.
-//        supportActionBar?.setDisplayHomeAsUpEnabled(true) // This is to use the home back button.
-//
-//        toolbar_add_place.setNavigationOnClickListener {
-//            onBackPressed()
-//        }
+        val toolbar_add_place = findViewById(R.id.toolbar_add_place) as Toolbar
+        et_date = findViewById(R.id.et_date)
+        val btn_save: Button = findViewById(R.id.btn_save)
+
+        //back button
+        setSupportActionBar(toolbar_add_place) // Use the toolbar to set the action bar.
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) // This is to use the home back button.
+        toolbar_add_place.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+        dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, month)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateDateInView()
+        }
+
+        et_date?.setOnClickListener(this)
     }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.et_date -> {
+                DatePickerDialog(
+                    this@AddHappyPlaceActivity,
+                    dateSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)
+                ).show()
+            }
+        }
+
+    }
+
+    private fun updateDateInView() {
+        val myFormat = "dd.MM.yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
+
+        et_date?.setText(sdf.format(cal.time).toString())
+
+    }
+
 }
