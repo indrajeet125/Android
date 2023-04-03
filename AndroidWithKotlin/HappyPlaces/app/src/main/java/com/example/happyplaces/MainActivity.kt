999f36e3f78cf7164e9b439ae7b26dca135001e7
indrajeet125 <indrajeetyadav932001@gmail.com>
2023-04-03 15:36:20 +0530

    inital commit

--- AndroidWithKotlin/HappyPlaces/app/src/main/java/com/example/happyplaces/MainActivity.kt
+++ AndroidWithKotlin/HappyPlaces/app/src/main/java/com/example/happyplaces/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.happyplaces
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import de.hdodenhof.circleimageview.CircleImageView
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +17,12 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, AddHappyPlaceActivity::class.java)
             startActivity(intent)
         }
+
+        var iv_simple_image: CircleImageView=findViewById(R.id.iv_simple_image)
+        var iv_circluler_image:CircleImageView=findViewById(R.id.iv_circluler_image)
+
+        iv_simple_image.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.ic_action_add_24dp))
+        iv_simple_image.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.ic_action_add_24dp))
 
     }
 }
