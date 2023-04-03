60e1c3af2ceae1c960c6c65b98c6bbd67a3048a2
indrajeet <indrajeetyadav932001@gmail.com>
2023-04-03 14:22:38 +0530

    initial commit

--- AndroidWithKotlin/HappyPlaces/app/src/main/java/com/example/happyplaces/AddHappyPlaceActivity.kt
+++ AndroidWithKotlin/HappyPlaces/app/src/main/java/com/example/happyplaces/AddHappyPlaceActivity.kt
@@ -2,10 +2,19 @@ package com.example.happyplaces
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class AddHappyPlaceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_happy_place)
+
+//        val toolbar_add_place = findViewById(R.id.toolbar_add_place) as Toolbar
+//        setSupportActionBar(toolbar_add_place) // Use the toolbar to set the action bar.
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true) // This is to use the home back button.
+//
+//        toolbar_add_place.setNavigationOnClickListener {
+//            onBackPressed()
+//        }
     }
 }
